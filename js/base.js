8a766c11450a7f3cb9bc0d65df629d69cf404df9
zwyhome <799378666@qq.com>
2017-01-09 23:44:17 +0800

    更新

---
+++ js/base.js
@@ -0,0 +1,10 @@
+/**
+ *百度统计
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?0f11cdc7d1b2e41d26a181b4f729741a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
---
+++ js/nav.js
@@ -0,0 +1,43 @@
+var navScript = $("script").last();
+$(function(){
+    (function(){
+        var navbar = $("<nav>").addClass("navbar navbar-default").insertBefore($(document.body.firstChild));
+        navbar.attr("role","navigation");
+
+        var container = $("<div>").addClass("container-fluid").appendTo(navbar);
+        var navHeader = $("<div>").addClass("navbar-header").appendTo(container);
+        var  navBrand = $("<a>BBS</a>").addClass("navbar-brand").attr("href",".").appendTo(navHeader);
+
+        var collapse = $("<div>").addClass("collapse navbar-collapse").appendTo(container);
+        var nav = $("<div>").addClass("nav navbar-nav").appendTo(collapse);
+
+        var menus =[
+            {
+                text:"首页",
+                flag:"home",
+                href:"."
+            },{
+                text:"关于",
+                flag:"about",
+                href:"about.html"
+            }
+        ];
+        var flag = navScript.attr("flag");
+        menus.forEach(function(menu){
+            var li = $("<li>").appendTo(nav);
+            $("<a>"+menu.text+"</a>").attr("href",menu.href).attr("target",menu.target).appendTo(li);
+            if(flag && menu.flag == flag){
+                li.addClass("active");
+            }
+        })
+
+    })();
+
+})
+
+
+
+
+
+
+
